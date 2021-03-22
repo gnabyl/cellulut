5d3cf0fedce4e22668f710acfdbd2060017ed8f1
vierabap <baptiste.viera@etu.utc.fr>
2021-03-22 16:03:28 +0100

    ajout test

---
+++ test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Baptiste Viera on 22/03/2021.
+//
+
+#include "test.h"
---
+++ test.h
@@ -0,0 +1,14 @@
+//
+// Created by Baptiste Viera on 22/03/2021.
+//
+
+#ifndef LO21_TEST_H
+#define LO21_TEST_H
+
+
+class test {
+
+};
+
+
+#endif //LO21_TEST_H
