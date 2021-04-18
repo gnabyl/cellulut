9baef899ee41b3e10208a93da68010ebcc868e5f
uruk380 <baptiste.viera@etu.utc.fr>
2021-04-18 13:02:23 +0200

    Simulator.cpp & Simulator.h ajout de la structure gobale

---
+++ Simulator.cpp
@@ -0,0 +1,128 @@
+//
+// Created by Baptiste on 18/04/2021.
+//
+
+#include "Simulator.h"
+
+
+///-------------------------------------------------------------------
+/// Classe Simulator
+///-------------------------------------------------------------------
+
+
+Simulator::Simulator(const Automate& a, size_t buf):automata(a),bufferSize(buf)
+{
+    grids = new Grid*[bufferSize];
+    for(size_t i=0; i<bufferSize; i++) grids[i]=nullptr;
+}
+
+Simulator::Simulator(const Automata& a, const Grid& startG, size_t buf):automata(a),startGrid(&startG),bufferSize(buf)
+{
+    grids = new Grid*[bufferSize];
+    for(size_t i=0; i<bufferSize; i++) grids[i]=nullptr;
+    grids[0]=new Grid(startG);
+}
+
+void Simulator::setStartGrid(const Grid& g)
+{
+    startGrid = &g;
+    // reset(); // Important à revoir
+}
+// important j'ai laissé comme cela (séparation run et next) à voir si c'est bien ou pas
+void Simulator::run(size_t nbSteps)
+{
+    for(size_t i=0; i<nbSteps; i++) next();
+}
+
+void Simulator::next()
+{
+    if(startGrid == nullptr) throw AutomateException("grille de départ indefinie");
+    //rang++; important à regarder
+    //build(rang%nbMaxEtats); important à regarder
+    automata.applyTransition(/* *grids[(rang-1)%bufferSize],*grids[rang%bufferSize]*/);
+}
+/*
+const Etat& Simulateur::dernier() const
+{
+    return *etats[rang%nbMaxEtats];
+}
+
+size_t Simulateur::getRangDernier() const
+{
+    return rang;
+}
+
+void Simulateur::reset()
+{
+    if(depart == nullptr) throw AutomateException("etat depart indefini");
+    rang=0;
+    build(0);
+    *etats[0]=*depart;
+}
+*/
+Simulator::~Simulator()
+{
+    for(size_t i=0; i<bufferSize; i++) delete grids[i];
+    delete[] grids;
+}
+/*
+void Simulateur::build(size_t c)
+{
+    if (c>=nbMaxEtats) throw AutomateException("erreur taille buffer");
+    if(grids[c] == nullptr) grids[c] = new Etat;
+}
+*/
+
+Simulator::Iterator Simulator::getIterator()
+{
+    return Iterator(this);
+}
+/*
+Simulateur::ConstIterator Simulateur::getIterator() const
+{
+    return ConstIterator(this);
+}
+
+Simulateur::ConstIterator Simulateur::getConstIterator() const
+{
+    return ConstIterator(this);
+}
+
+*/
+
+///-------------------------------------------------------------------
+/// Classe Simulator::Iterator
+///-------------------------------------------------------------------
+
+Simulator::Iterator::Iterator()
+{
+
+}
+
+Simulator::Iterator::Iterator(Simulator* s):sim(s),gridID(s->rang) // important regarder l'histoire de rang
+{
+
+}
+
+bool Simulator::Iterator::isDone() const
+{
+    return sim == nullptr || gridID==-1 || gridID == sim->rang-sim->bufferSize; // important
+}
+
+void Simulator::Iterator::nextGrid()
+{
+    if(isDone()) throw AutomateException("Erreur: next sur un iteraror fini!");
+    gridID++;
+}
+
+void Simulator::Iterator::previousGrid()
+{
+    if(isDone()) throw AutomateException("Erreur: previous sur un iteraror fini!");
+    gridID--;
+}
+
+Grid& Simulator::Iterator::getCurrentGrid() const
+{
+    if(isDone()) throw AutomateException("Erreur: current sur un iteraror fini!");
+    return *sim->grids[gridID%sim->bufferSize];
+}
---
+++ Simulator.h
@@ -0,0 +1,97 @@
+//
+// Created by Baptiste on 18/04/2021.
+//
+
+#ifndef LO21_SIMULATOR_H
+#define LO21_SIMULATOR_H
+
+
+class Simulator {
+private:
+    const Automata &automata;
+    size_t bufferSized;
+    const Grid *startGrid = nullptr; // il faut ajouter un pointeur dans l'UML
+    Grid **grids = nullptr;
+    // Gridfactory gridFactory
+    // size_t rang=0;
+
+    //void build(size_t c);
+
+    Simulator(const Simulator &s) = delete;
+
+    Simulator &operator=(const Simulator &s) = delete;
+
+
+public:
+    Simulator(const Automata &a, size_t buf = 2);
+
+    Simulator(const Automata &a, const Grid &startG, size_t buf = 2);
+
+    ~Simulator();
+
+    void setStartGrid(const Grid &g);
+
+    Grid getStartGrid() const;
+
+    void run(size_t nbSteps = 1);
+    void next(); // important à voir si on utilise ou pas
+
+    unsigned int frequency; // vitesse de passage d'une génération à une autre
+
+    // Important A voir s'il faudra l'implémenter
+
+    //const Etat& dernier() const;
+    //size_t getRangDernier() const;
+    //void reset();
+
+    class Iterator {
+        friend class Simulator;
+
+    private:
+        Simulator *sim = nullptr;
+        int gridID = 0;
+
+        Iterator(Simulator *s);
+
+    public:
+        Iterator();
+
+        bool isDone() const;
+
+        void previousGrid();
+
+        void nextGrid();
+
+        Grid &current() const;
+
+        //operator!=(): bool
+    };
+
+    Iterator getIterator();
+}
+
+    // Important besoin d'un const iterator ou pas ?
+    // Je pense que non puisque l'on peut modifier notre grille lorsque l'on fait un parcourt
+    /*
+    class ConstIterator
+    {
+        friend class Simulateur;
+
+    private:
+        const Simulateur* sim = nullptr;
+        int i=0;
+        ConstIterator(const Simulateur* s);
+
+    public:
+        ConstIterator();
+        bool isDone() const;
+        void next();
+        const Etat& current() const;
+    };
+
+    ConstIterator getIterator() const;
+    ConstIterator getConstIterator() const;
+};
+*/
+
+#endif //LO21_SIMULATOR_H
