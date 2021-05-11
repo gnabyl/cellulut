195b6aadca41a50bbdd10f84f60ef42e45bb5031
Sacha Benarroch <sacha.benarroch@etu.utc.fr>
2021-05-11 19:26:21 +0200

    Update grid.h

--- grid.h
+++ grid.h
@@ -2,23 +2,24 @@
 
 #ifndef LO21_GRID_H
 #define LO21_GRID_H
-#include cellstate.h
-#include cell.h
+#include "cellstate.h"
+#include "cell.h"
 
 class Grid{
 private:
-cell **cells=nullptr;
-int width;
-int height;
+    Cell **cells=nullptr;
+    int width;
+    int height;
 
 public:
-Grid(cellstate s, int w=0,int h=0);
-Grid(const Grid& g);
-~Grid();
-Grid& operator= (const Grid& g);
+    Grid(cellstate s, int w=0,int h=0);
+    Grid(const Grid& g);
+    ~Grid();
+    Grid& operator= (const Grid& g);
 
-void setCell(cellstate c,int x,int y);
-cell& getCell(int x , int y) const;
-int getwidth() const;
-int getheight() const;
+    void setCell(cellstate c,int x,int y);
+    cell& getCell(int x , int y) const;
+    int getwidth() const;
+    int getheight() const;
+};
 #endif //LO21_GRID_H
