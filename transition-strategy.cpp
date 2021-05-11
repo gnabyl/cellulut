343f5b4e8d454d1150e37afecbffb9fa3f8c29ae
Sacha Benarroch <sacha.benarroch@etu.utc.fr>
2021-05-11 15:56:35 +0200

    [feat][api] add Brian's Brain transition strategy

--- transition-strategy.cpp
+++ transition-strategy.cpp
@@ -1,24 +1,33 @@
 #include "transition-strategy.h"
 
-void TransitionContext::setStrategy(TransitionStrategy* s){
-	if(strategy != nullptr)
-		delete strategy;
-	strategy = s;
-}
-
 Cell TotalisticTransition::calcNextCell(const Cell c, const Cell** neighbors, int neighborSize, CellState** availableStates, int nbStates) const{
 	/*Find a way to implement any totalistic transition. Remember that it only takes into account the sum of the neighbor's current states' IDs.*/
+
+	//Step 1 : identify the state of each cell in the neighborhood and sum them.
+	int sum = 0;
+	for(int i=0; i<neighborSize; i++){
+		int j=0;
+		while(availableStates[j]!=neighbors[i]->state.id) j++;
+		sum += j;
+	}
+	int newState = 0;
+	for(int i=0; i<neighborSize*(nbStates-1); i++)
+		if(result[i]==sum)
+			newState = i;//Remplacer par un while.
+	Cell* c = new Cell(/*Initialiser avec l'état obtenu dans availableStates[newState]*/);
+	return c;
 }
 
 /*
+FONCTION DE TRANSITION DU JEU DE LA VIE
 Caractéristiques :
 - Règle totalistique
-- Taille du voisinage : 8 -> moyen de le vérifier ? On rajoute un argument ?
-- Nombre d'états possibles : 2 -> on rajoute un argument.
+- Taille du voisinage : 8
+- Nombre d'états possibles : 2
 Suppositions :
 - Les id des états sont forcément 0 ou 1. On pourra gérer ça avec des modulo ensuite si besoin.
 */
-Cell GOLTransition::calcNextCell(const Cell c, const Cell** neighbors, int neighborSize, CellState** availableStates, int nbStates) const override{
+Cell GOLTransition::calcNextCell(const Cell c, const Cell** neighbors, int neighborSize, CellState** availableStates, int nbStates) const{
 	if(neighborSize != 8)
 		throw TransitionException("La taille du voisinage choisi est incohérente avec la fonction de transition utilisée : Game of Life exige un voisinage de 8 cellules.\n");
 	if(nbStates != 2)
@@ -27,11 +36,42 @@ Cell GOLTransition::calcNextCell(const Cell c, const Cell** neighbors, int neigh
 	for(int i=0; i<neighborSize; i++){
 		sum += (availableStates[0]->id == neighbors[i]->state.id) ? 0 : 1;
 	}
-	if(c.state == 0 && sum == 3)
+	if(c.State.getid() == availableStates[0]->id && sum == 3)
 		return new Cell(availableStates[1]);
-	if(c.state == 1 && (sum == 2 || sum == 3))
+	if(c.State.getid() == availableStates[1]->id && (sum == 2 || sum == 3))
 		return new Cell(availableStates[1]);
-	if(c.state == 1 && (sum<2 || sum>3))
+	if(c.State.getid() == 1=availableStates[1]->id && (sum<2 || sum>3))
 		return new Cell(availableStates[0]);
-	else return new Cell(c.state);
+	else return new Cell(c.State);
+}
+
+/*
+FONCTION DE TRANSITION DE BRIAN'S BRAIN
+Caractéristiques :
+- Règle totalistique
+- Taille du voisinage : 8
+- Nombre d'états possibles : 3
+Suppositions :
+- Les id des états sont forcément 0 (repos), 1 (réfractaire) ou 2 (excitée).
+*/
+
+Cell BBTransition::calcNextCell(const Cell c, const Cell** neighbors, int neighborSize, CellState** availableStates, int nbStates) const{
+	if(neighborSize!=8)
+		throw TransitionException("La taille du voisinage choisi est incohérente avec la fonction de transition utilisée : Brian's Brain exige un voisinage de 8 cellules.\n");
+	if(nbStates!=2)
+		throw TransitionException("Le nombre d'états de l'automate est incohérent avec la règle de transition choisie : Brian's Brain ne fonctionne qu'avec 3 états possibles.\n");
+	//Toute cellule excitée devient réfractaire.
+	if(c.State.getid()==availableStates[2]->id)
+		return new Cell(availableStates[1]);
+	//Toute cellule réfractaire devient au repos.
+	if(c.State.getid()==availableStates[1]->id)
+		return new Cell(availableStates[0]);
+	//Pour une cellule au repos, on s'intéresse au nombre de cellules excitées dans le voisinage.
+	int sum = 0;
+	for(int i=0;i<neighborSize;i++){
+		if(availableStates[2]->id == neighbors[i]->state.id) sum+=1
+	}
+	//Toute cellule au repos ayant exactement 2 voisinges excitées devient excitée.
+	if(c.State.getid()==availableStates[0]->id && sum==2)
+		return new Cell(availableStates[2]);
 }
--- transition-strategy.h
+++ transition-strategy.h
@@ -1,11 +1,18 @@
+#ifndef TRANSITION_STRATEGY_H
+#define TRANSITION_STRATEGY_H
+
 #include <iostream>
 #include <string>
+#include "automata.h"
+#include "neighbor-strategy.h"
+#include "cellstate.h"
+#include "grid.h"
 
 class TransitionException{
 	std::string info;
 public:
 	TransitionException(std::string s): info(s){}
-	getInfo(return info;)
+	getInfo(){return info;}
 };
 
 ////Implementation of the Strategy design pattern./////
@@ -17,33 +24,28 @@ public:
 	virtual Cell calcNextCell(const Cell, const Cell**, int, CellState**, int) const = 0;
 };
 
-class TransitionContext{ //context
-	TransitionStrategy* strategy;
-public:
-	TransitionContext(TransitionStrategy* s = nullptr) : strategy(s){}
-	void setStrategy(TransitionStrategy* s);
-	Cell NextCell(const Cell c, const Cell** neighbors, int neighborSize, CellState** availableStates, int nbStates) const{strategy.calcNextCell(c, neighbors, neighborSize, availableStates, nbStates);}
-	~TransitionContext(){delete strategy;}
-};
 
 class TotalisticTransition : public TransitionStrategy{
-	/*Add here some properties that provide a description of the transition rule*/
 	virtual Cell calcNextCell(const Cell, const Cell**, int, CellState**, int) const;
 public:
-	TotalisticTransition(/*Initialize the properties described above.*/);
+	TotalisticTransition(){}
 };
 
 
 class StochasticTransition : public TransitionStrategy{
-
+	virtual Cell calcNextCell(const Cell, const Cell**, int, CellState**, int) const;
 };
 
 class OuterTotalisticTransition : public TotalisticTransition{
-
+	virtual Cell calcNextCell(const Cell, const Cell**, int, CellState**, int) const;
 };
 
 ///Concrete strategies///
 
 class GOLTransition : public TotalisticTransition{
-	Cell calcNextCell(const Cell, const Cell**, int, CellState**, int) const override;
+	Cell calcNextCell(const Cell, const Cell**, int, CellState**, int) const;
 };
+
+class BBTransition : public TotalisticTransition{
+	Cell calcNextCell(const Cell, const Cell**, int, CellState**, int) const;
+}
