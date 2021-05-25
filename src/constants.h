8601edbc9741f56eb1b2be5256f77dfdc9b9306f
Bang Nguyen Le Ly <le-ly-bang.nguyen@etu.utc.fr>
2021-05-25 22:11:54 +0200

    [feat][app] add image icons support for window, tested

--- src/constants.h
+++ src/constants.h
@@ -4,11 +4,11 @@
 #include <QString>
 #include <QDir>
 
-const QString PREV_BTN_ICON_PATH = QDir(":assets/").filePath("previous-button.png");
-const QString PLAY_BTN_ICON_PATH = QDir(":assets/").filePath("play-button.png");
-const QString PAUSE_BTN_ICON_PATH = QDir(":assets/").filePath("pause-button.png");
-const QString RESET_BTN_ICON_PATH = QDir(":assets/").filePath("reset-button.png");
-const QString RANDOM_BTN_ICON_PATH = QDir(":assets/").filePath("random-button.png");
-const QString NEXT_BTN_ICON_PATH = QDir(":assets/").filePath("next-button.png");
+const QString PREV_BTN_ICON_PATH = QString(":/assets/previous-button.png");
+const QString PLAY_BTN_ICON_PATH = QString(":/assets/play-button.png");
+const QString PAUSE_BTN_ICON_PATH = QString(":/assets/pause-button.png");
+const QString RESET_BTN_ICON_PATH = QString(":/assets/reset-button.png");
+const QString RANDOM_BTN_ICON_PATH = QString(":/assets/random-button.png");
+const QString NEXT_BTN_ICON_PATH = QString(":/assets/next-button.png");
 
 #endif // CONSTANTS_H
