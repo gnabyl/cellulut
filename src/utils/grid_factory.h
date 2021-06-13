18a74d787b34097391d2fa715450aff01b55796a
Sacha Benarroch <sacha.benarroch@etu.utc.fr>
2021-06-13 19:46:01 +0200

    [doc] update DOxygen doc

--- src/utils/grid_factory.h
+++ src/utils/grid_factory.h
@@ -10,9 +10,20 @@ enum GridType {
     RANDOM
 };
 
+/**
+ * @brief The GridFactory class implémentation du design pattern fabrique pour générer des pavages
+ */
 class GridFactory {
   public:
     GridFactory();
+    /**
+     * @brief generateGrid génère un pavage à utiliser par un simulateur
+     * @param type discrimine les grilles vides et les grilles déterminées aléatoirement
+     * @param width largeur de la grille
+     * @param height hauteur de la grille
+     * @param automata automate utilisant la grille générée
+     * @return retourne un pavage
+     */
     Grid* generateGrid(GridType type, int width, int height, Automata* automata) const;
 };
 
